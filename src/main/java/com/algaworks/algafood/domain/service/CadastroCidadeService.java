fae4b91eb396e2aa696cc096d840339c9c760eb4
Igor Martins <dev.igor.martins@gmail.com>
2021-03-24 10:48:49 -0300

    Criação da camada service para cidades, dsenvolvimento da rest controladora cidades com os recursos listar e buscar por id

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCidadeService.java
@@ -0,0 +1,34 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class CadastroCidadeService {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	public Cidade salvar(Cidade cidade) {
+		Long estadoId = cidade.getEstado().getId();
+		Estado estado = estadoRepository.buscar(estadoId);
+		
+		if(estado == null) {
+			throw new EntidadeNaoEncontradaException(String.format("Não existe estado cadastro com esse código %d", estadoId));
+		}
+		
+		cidade.setEstado(estado);
+		
+		return cidadeRepository.salvar(cidade);
+	}
+	
+}
--- src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CidadeRepositoryImpl.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
@@ -26,11 +27,13 @@ public class CidadeRepositoryImpl implements CidadeRepository {
 		return manager.find(Cidade.class, id);
 	}
 
+	@Transactional
 	@Override
 	public Cidade salvar(Cidade cidade) {		
 		return manager.merge(cidade);
 	}
 
+	@Transactional
 	@Override
 	public void remover(Cidade cidade) {
 		cidade = buscar(cidade.getId());
