0e916c4e2bfb10cf6c06c01876e16e4c8bf45ae5
Igor Martins <dev.igor.martins@gmail.com>
2021-03-24 00:35:26 -0300

    Criação de recurso para atualização de restaurante e recurso para busca de estado por ID

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,18 @@ public class EstadoController {
 	public List<Estado> listar(){
 		return estadoRepository.listar();
 	}
+	
+	@GetMapping("/{estadoId}")
+	public ResponseEntity<Estado> buscar(@PathVariable Long estadoId){
+		Estado estado = estadoRepository.buscar(estadoId);
+		
+		if(estado != null) {
+			return ResponseEntity.ok(estado);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 	
 }
